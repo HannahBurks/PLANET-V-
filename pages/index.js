f04e936c39a05727f369010fae821c54d9cea2d9
Hannah Burks <105048825+HannahBurks@users.noreply.github.com>
2022-09-12 12:32:41 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export const getServerSideProps = async (ctx) => {
     admin = true;
   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get("https://main--planet-v-takeaway.netlify.app//api/products");
   return {
     props: {
       pizzaList: res.data,

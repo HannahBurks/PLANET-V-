8e3047e3ec36406eaa0a1a3e2fc9d3b6bbbb8ec6
Hannah Burks <105048825+HannahBurks@users.noreply.github.com>
2022-09-12 17:15:42 +0100

    Update Add.jsx

--- components/Add.jsx
+++ components/Add.jsx
@@ -42,7 +42,7 @@ const Add = ({setClose}) => {
             img: url,
           };
     
-          await axios.post("http://localhost:3000/api/products", newProduct);
+          await axios.post("https://main--planet-v-takeaway.netlify.app/api/products", newProduct);
           setClose(true);
         } catch (err) {
           console.log(err);
